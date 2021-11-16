ae834f7444d972a65fc9a07885a28b449cab001a
masanam <masanam@yahoo.com>
2021-11-16 11:18:41 +0700

    update remote address ip variable

--- database/migrations/2021_11_16_030836_create_tasks_table.php
+++ database/migrations/2021_11_16_030836_create_tasks_table.php
@@ -16,7 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('ipaddress');
+            $table->string('ipaddress')->default('127.0.0.1');
             $table->string('label');
             $table->text('details'); 
             $table->timestamps();
